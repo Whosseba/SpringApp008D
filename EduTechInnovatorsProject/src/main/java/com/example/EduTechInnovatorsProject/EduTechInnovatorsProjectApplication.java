9cd8a6682e688f4ca6e2dd0ad5dd3d0aac8c8faf
whosseba <s.barriga@duocuc.cl>
2025-05-10 00:16:35 -0400

    mi primer commit :)

---
+++ EduTechInnovatorsProject/src/main/java/com/example/EduTechInnovatorsProject/EduTechInnovatorsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduTechInnovatorsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTechInnovatorsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTechInnovatorsProjectApplication.class, args);
+	}
+
+}
