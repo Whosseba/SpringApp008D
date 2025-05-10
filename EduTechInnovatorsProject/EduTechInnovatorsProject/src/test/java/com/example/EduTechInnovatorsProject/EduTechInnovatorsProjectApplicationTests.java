abc82ed6cbf09cafdcdf5eeccd3b22bb5b7d3039
Sebastian Barriga <s.barriga@duocuc.cl>
2025-05-09 23:40:21 -0400

    Add files via upload

---
+++ EduTechInnovatorsProject/EduTechInnovatorsProject/src/test/java/com/example/EduTechInnovatorsProject/EduTechInnovatorsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EduTechInnovatorsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTechInnovatorsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
